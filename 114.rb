716ae9f15058cca4025b27641907704cced529e4
s3cr3t-cyb3r <lancesanchez2010@gmail.com>
2026-03-05 18:21:03 +1000

    Create 114.rb

---
+++ 114.rb
@@ -0,0 +1,21 @@
+# QUESTION 1
+# In this code, the variable z is printed out on lines 4 and 6. 
+# Predict what will print out to the terminal, then run the code to see if you were right. 
+# Explain in your own words exactly why those values were printed to the terminal.
+x = 40
+y = 3
+z = x + y
+puts z
+x = 60
+puts z
+
+# QUESTION 2
+# Write comments for the remaining lines to describe in your own words 
+# what is happening line by line with the following code.
+mars = 4               # Make a variable called mars and set the value to 4
+mars = 2 * mars        # Multiply the value in mars by 2
+pluto = 7              # Make a variable called pluto and set the value to 7
+mars = mars - pluto    # Subtract pluto's value from mars's value and make it mars's new value
+pluto = pluto + mars   # Add pluto's value to mars's new value and make it pluto's new value
+puts mars              # Print mars's value on the terminal
+puts pluto             # Print pluto's value on the terminal
