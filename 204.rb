076187f6f43e53da78d7ed9abdef2d8e28285a24
s3cr3t-cyb3r <lancesanchez2010@gmail.com>
2026-03-12 19:54:08 +1000

    Create 204.rb

---
+++ 204.rb
@@ -0,0 +1,7 @@
+# QUESTION 1
+# Paste this link in your browser to make a copy of the Career Inventory Google document: 
+# https://docs.google.com/document/d/1MMO_Z7hJWEuXZ28pYFHrWp0wm-UIZQZiBpidkBSwm3w/edit?usp=sharing   
+# Fill in the answers for each question in the Career Inventory.
+# Change the sharing settings of the document to "Anyone with the link can view" 
+# Copy the link to your Career Inventory Google document and paste it below.
+# https://docs.google.com/document/d/1t9dg4q9naXd-qz76ujRSULuIdhYxfcVIimtW9DcnHbw/edit?usp=sharing
