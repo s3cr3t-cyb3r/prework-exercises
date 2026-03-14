2096a940dd5dc2c19026eef93e19067068920335
s3cr3t-cyb3r <lancesanchez2010@gmail.com>
2026-03-14 13:17:33 +1000

    Day 10 Submission

---
+++ 231.rb
@@ -0,0 +1,15 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Write a while loop that prints the numbers 100, 90, 80, 70, 60, ..., 10, 0.
+index =  100
+while index != 0
+    puts index
+    index -= 10
+end
+# Were you able to easily solve the problem from memory? Yes
+# If so, describe the next skill you will start to practice tomorrow. Arrays and hashes
+# If not, describe the part you got stuck on that requires more practice.
+
+
---
+++ 232.rb
@@ -0,0 +1,16 @@
+# QUESTION 1
+# Write code that prints all the years between 1980 to 2020 (including 1980 and 2020).
+year = 1980
+while year != 2021
+    puts year
+    year += 1
+end
+
+# QUESTION 2
+# The following code is supposed to print the numbers 1 through 10, but it has errors.
+# Fix the errors with the code below.
+count = 0
+while count != 10
+    puts count + 1
+    count += 1
+end
