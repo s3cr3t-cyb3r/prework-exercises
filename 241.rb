6052fb54b710f3419af9396708e1f6d1ae48a3b1
s3cr3t-cyb3r <lancesanchez2010@gmail.com>
2026-03-20 21:12:01 +1000

    Day 11 submission

---
+++ 241.rb
@@ -0,0 +1,13 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Create an array to store 4 island fruits. Then change the second fruit to a vegetable and print the array on one line.
+food = ["guava", "strawberry", "mango", "coconut"]
+food[1] = "broccoli"
+pp food
+# Were you able to easily solve the problem from memory? yes
+# If so, describe the next skill you will start to practice tomorrow. more arrays and hashes
+# If not, describe the part you got stuck on that requires more practice.
+
+
---
+++ 242.rb
@@ -0,0 +1,12 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+words = ["superficial", "temporary", "figment"]
+words.push("forgotten")
+pp words
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+words_hash = {"imaginary" => "extisting only in one's imagination; fictional", "cynical" => "doubtful of people's sincerity"}
+words_hash["insignificant"] = "not significant; unimportant"
+pp words_hash
